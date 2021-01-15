0bab8e48484338d1de2aedefc8a76af972a9ff8f
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-01-14 20:15:16 -0500

    Fixed ultrasonic_driver to hopefully read i2c transmission correctly

--- ultrasonic_driver.py
+++ ultrasonic_driver.py
@@ -21,17 +21,18 @@ def readI2C(address):
 
 def main():
     while True:
-        while(readI2C(SLAVE_ADDR) < 255):              #255 is the start byte, so if we read in the middle of a transmission, wait until next start
-            for bcount in range(5):
-                distance[bcount] = readI2C(SLAVE_ADDR) #Put each distance in the list in its respective position
-                # if(distance[bcount] < 100):
-                #     motor_controller.stop()
-                #     time.sleep(1)
-            print("0: "+str(distance[0])+
-                  " 1: "+str(distance[1])+
-                  " 2: "+str(distance[2])+
-                  " 3: "+str(distance[3])+
-                  " 4: "+str(distance[4]))
-            time.sleep(.200)                           #Delay for 200ms
+        while(readI2C(SLAVE_ADDR) < 255): #255 is the start byte, so if we read in the middle of a transmission, wait until next start
+            print("Waiting")                
+        for bcount in range(5):
+            distance[bcount] = readI2C(SLAVE_ADDR) #Put each distance in the list in its respective position
+            # if(distance[bcount] < 100):
+            #     motor_controller.stop()
+            #     time.sleep(1)
+        print("0: "+str(distance[0])+
+                " 1: "+str(distance[1])+
+                " 2: "+str(distance[2])+
+                " 3: "+str(distance[3])+
+                " 4: "+str(distance[4]))
+        time.sleep(.200)                           #Delay for 200ms
 
 main()
