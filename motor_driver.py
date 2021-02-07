b5a19ef05f790059025318481eef5c1504900150
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-02-06 23:27:18 -0500

    Ease of testing

--- motor_driver.py
+++ motor_driver.py
@@ -27,8 +27,6 @@ class MotorDriver():
         self.pwm = [GPIO.PWM(self.RIGHT_MOTOR, 50), GPIO.PWM(self.LEFT_MOTOR, 50)]
         self.pwm[0].start(0)
         self.pwm[1].start(0)
-        print(self.pwm[0])
-        print(self.pwm[1])
 
     # Move both motors forwards at speed for duration
     def fwd_bwd(self, spd, dur, dir):
@@ -41,10 +39,8 @@ class MotorDriver():
 
         right_speed = ((spd - (-1))/2)*100
         left_speed = right_speed
-        self.pwm[0].ChangeDutyCycle(right_speed)
-        self.pwm[1].ChangeDutyCycle(left_speed)
-        print(right_speed)
-        print(left_speed)
+        self.pwm[0].ChangeDutyCycle(100)
+        self.pwm[1].ChangeDutyCycle(100)
         time.sleep(dur)
         self.stop()
 
