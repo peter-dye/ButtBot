e873f7cdb8c3d501bcef41a827cf245b1ceaea94
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-02-16 13:15:48 -0500

    single slave test with a char

--- slave_test.py
+++ slave_test.py
@@ -7,19 +7,21 @@ bus = smbus2.SMBus(1)
 
 #Reads the distances from the slave
 def writeI2C(address, speed, duration, direction):
-    bus.write_byte_data(address, 0, speed)                  
-    time.sleep(.100) 
-    bus.write_byte_data(address, 0, duration)
-    time.sleep(.100)    
-    if direction == 'fwd':
-        dir = 0
-    elif direction == 'bwd':
-        dir = 1
-    elif direction == 'left':
-        dir = 2
-    elif direction == 'right':
-        dir = 3
-    bus.write_byte_data(address, 0, dir)                              
+    #speed = speed*255
+    #bus.write_byte_data(address, 0, speed)                  
+    #time.sleep(.100) 
+    #bus.write_byte_data(address, 0, duration)
+    #time.sleep(.100)    
+    #if direction == 'fwd':
+    #    dir = 0
+    #elif direction == 'bwd':
+    #    dir = 1
+    #elif direction == 'left':
+    #    dir = 2
+    #elif direction == 'right':
+    #    dir = 3
+    #bus.write_byte_data(address, 0, dir)                     
+    bus.write_byte_data(address, 0, "A")         
 
 def main():
     i = 0
