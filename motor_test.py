c177aba6ebcc5c449783bb08ee909632773f886e
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-02-15 19:36:52 -0500

    motor_test changed to try out pwm

--- motor_test.py
+++ motor_test.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
-output_pin = 15
+output_pin = 33
 
 def main():
     # Pin Setup:
@@ -9,26 +9,25 @@ def main():
     GPIO.setmode(GPIO.BOARD)
     # set pin as an output pin with optional initial state of HIGH
     GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
-    GPIO.setup(13, GPIO.OUT, initial=GPIO.HIGH)
-    #p = GPIO.PWM(output_pin, 50)
-    #val = 100
-    #incr = 100
-    #p.start(val)
+    GPIO.setup(15, GPIO.OUT, initial=GPIO.HIGH)
+    p = GPIO.PWM(output_pin, 50)
+    val = 100
+    incr = 100
+    p.start(val)
     while True:
-        pass
-    #print("PWM running. Press CTRL+C to exit.")
-    #try:
-    #    while True:
-    #        time.sleep(3)
-    #        if val >= 100:
-    #            incr = -incr
-    #        if val <= 0:
-    #            incr = -incr
-    #        val += incr
-    #        p.ChangeDutyCycle(val)
-    #finally:
-    #    p.stop()
-    #    GPIO.cleanup()
+        print("PWM running. Press CTRL+C to exit.")
+        try:
+            while True:
+                time.sleep(3)
+                if val >= 100:
+                    incr = -incr
+                if val <= 0:
+                    incr = -incr
+                val += incr
+                p.ChangeDutyCycle(val)
+        finally:
+            p.stop()
+            GPIO.cleanup()
 
 if __name__ == '__main__':
     main()
