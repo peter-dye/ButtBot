c99330c9ad71d5bf5e4690608386362377d07300
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-02-16 15:44:30 -0500

    another test

--- slave_test.py
+++ slave_test.py
@@ -21,11 +21,11 @@ def writeI2C(address, speed, duration, direction):
     #elif direction == 'right':
     #    dir = 3
     #bus.write_byte_data(address, 0, dir)                     
-    bus.write_byte_data(address, 0, 9) 
+    bus.write_byte_data(address, 0, 10) 
     time.sleep(0.100)
-    bus.write_byte_data(address, 0, 8) 
+    bus.write_byte_data(address, 0, 11) 
     time.sleep(0.100)
-    bus.write_byte_data(address, 0, 7) 
+    bus.write_byte_data(address, 0, 12) 
     time.sleep(0.100)
 
 def main():
