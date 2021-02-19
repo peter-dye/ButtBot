2b0a71ec2066b0ae5343ba80a8481d1c1849ddf1
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-02-19 15:56:04 -0500

    errorfix

--- ultrasonic_driver.py
+++ ultrasonic_driver.py
@@ -15,7 +15,7 @@ class UltrasonicDriver():
     def readI2C(self, address):
         bval = 0                                #Temp variable to store the byte read from the bus
         time.sleep(.100)                        #Pause for 100ms
-        bval = bus.read_byte_data(address, 0)   #Read the byte from the bus
+        bval = self.bus.read_byte_data(address, 0)   #Read the byte from the bus
         return bval                             #Return read byte
 
 
