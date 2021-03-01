167feb0957137d05fcef3178d62c0bafbcd5a32b
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-03-01 13:48:28 -0500

    Removed code converting a float to ints in motor_driver, don't need to send duration over i2c. Updated Arduino code, as it doesn't need to read a duration

--- motor_driver.py
+++ motor_driver.py
@@ -10,36 +10,27 @@ class MotorDriver():
         self.bus = bus
 
     # Move both motors forwards at speed for duration
-    def fwd_bwd(self, spd, dur, dir):
-        dur_ones, dur_tenths, dur_hundreths = self.float_to_ints(dur)
+    def fwd_bwd(self, spd, dir):
         if dir == 'fwd':
             coded_dir = 1
         elif dir == 'bwd':
             coded_dir = 2
         spd_in_freq = int(spd*255)
-        motor_command = [spd_in_freq, dur_ones, dur_tenths, dur_hundreths, coded_dir]
-        print("boutta send: ", dur_ones, dur_tenths, dur_hundreths)
+        motor_command = [spd_in_freq, coded_dir]
         self.bus.write_i2c_block_data(SLAVE_ADDR, register=0, data=motor_command) 
 
     # Move right motor backwards, while moving left motor forwards until desired angle
-    def pivot(self, speed, dur, dir):
-        dur_ones, dur_tenths, dur_hundreths = float_to_ints(dur)
+    def pivot(self, speed, dir):
         if dir == 'left':
             coded_dir = 3
         if dir == 'right':
             coded_dir = 4
         spd_in_freq = 255
-        motor_command = [spd_in_freq, dur_ones, dur_tenths, dur_hundreths, coded_dir]
+        motor_command = [spd_in_freq, coded_dir]
         self.bus.write_i2c_block_data(SLAVE_ADDR, register=0, data=motor_command) 
 
     # Stop both motors
     def stop(self):
-        motor_command = [0,1,0]
+        motor_command = [0,0]
         self.bus.write_i2c_block_data(SLAVE_ADDR, register=0, data=motor_command)
-
-    def float_to_ints(self, float):
-        int_ones = math.floor(float)
-        int_tenths = math.floor((float - int_ones)*10)
-        int_hundreths = round((float - int_ones - int_tenths/10)*100)
-        return int_ones, int_tenths, int_hundreths
 
