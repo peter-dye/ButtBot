01ac098ab8a142e432aa62d22bbfbb234e47c758
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-03-02 18:00:08 -0500

    Created a new motor test without a thread, see if thread timing is the issue

---
+++ motor_test.py
@@ -0,0 +1,17 @@
+import time 
+import motor_driver
+import smbus2
+
+bus = smbus2.SMBus(0)
+mc = motor_driver.MotorDriver(bus)
+
+while True:
+    info = input('Enter Speed and Time ')
+    input_dims = info.split()
+    speed = int(input_dims[0])
+    dur = int(input_dims[1])
+    print("speed is", speed)
+    print("duration is", dur)
+    mc.fwd_bwd(speed, dur)
+    time.sleep(dur)
+    mc.stop()
