e5bd117f9e7e92eb4b2487194297a5e1db7e616f
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-03-08 17:49:42 -0500

    Trying to add motor thread back into working US thread

--- main_v1.py
+++ main_v1.py
@@ -26,19 +26,19 @@ us = ultrasonic_driver.UltrasonicDriver(ard_bus, us_buffer, us_lock)
 us_thread = Thread(target = us.write_to_mem)
 us_thread.start()
 
-#path_q = Queue()
-#path_thread = Thread(target = path_Q.put_cmd, args=(path_q,))
-#path_thread.start()
+path_q = Queue()
+path_thread = Thread(target = path_Q.put_cmd, args=(path_q,))
+path_thread.start()
 
 while True:
-    #if not path_q.empty():
-    #mtr_cmd = path_q.get()
-    #print("sending cmd to motor")
-    #mc.motor_send(mtr_cmd[0], mtr_cmd[1], mtr_cmd[2])
+    if not path_q.empty():
+        mtr_cmd = path_q.get()
+        print("sending cmd to motor")
+        mc.motor_send(mtr_cmd[0], mtr_cmd[1], mtr_cmd[2])
 
     distance = us.read_from_mem()
     for i in range(1):
         if distance[i] < 10:
             print("stop!")
             mc.stop()
-        print("US reading", distance[i])
+
