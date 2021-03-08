b7b7620cd21f912d19dd96d4c77c80b2222dff77
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-03-08 17:43:30 -0500

    debug statement

--- motor_driver.py
+++ motor_driver.py
@@ -19,6 +19,7 @@ class MotorDriver():
             coded_dir = 2
         spd_in_freq = int(spd*255)
         motor_command = [spd_in_freq, coded_dir]
+        print(motor_command)
         self.bus.write_i2c_block_data(SLAVE_ADDR, register=0, data=motor_command) 
 
     # Move right motor backwards, while moving left motor forwards until desired angle
