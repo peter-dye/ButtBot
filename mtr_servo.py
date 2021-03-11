b17710a33ce03b73c4242f07b715a59c6c5eb446
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-03-11 11:49:28 -0500

    trying to use servo driver board as GPIO

--- mtr_servo.py
+++ mtr_servo.py
@@ -1,7 +1,15 @@
 import board
 import busio
+import time
 import adafruit_pca9685
+
 i2c = busio.I2C(board.SCL, board.SDA)
 pca = adafruit_pca9685.PCA9685(i2c)
 
 pca.frequency = 1600
+
+mtr1_channel = pca.channels[8]
+
+mtr1_channel.duty_cycle = 0xFFFF
+time.sleep(1)
+mtr1_channel.duty_cycle = 0x0000
