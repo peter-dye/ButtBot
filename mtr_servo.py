ec930209967d975ee1b99c6e3b78958b2596cb44
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-03-11 11:45:13 -0500

    smbus doesn't work

--- mtr_servo.py
+++ mtr_servo.py
@@ -1,8 +1,7 @@
 import board
 import busio
 import adafruit_pca9685
-import smbus2 
-bus = smbus2.SMBus(1)
-pca = adafruit_pca9685.PCA9685(bus)
+i2c = busio.I2C(board.SCL, board.SDA)
+pca = adafruit_pca9685.PCA9685(i2c)
 
 pca.frequency = 1600
