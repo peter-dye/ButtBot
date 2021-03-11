7d89071b6a798052c7573b1cf6eea44c7480327f
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-03-11 12:21:26 -0500

    seeing if I can control servos at same time as motor

--- mtr_servo.py
+++ mtr_servo.py
@@ -3,6 +3,7 @@ import busio
 import time
 import adafruit_pca9685
 
+
 i2c = busio.I2C(board.SCL, board.SDA)
 pca = adafruit_pca9685.PCA9685(i2c)
 
@@ -10,8 +11,11 @@ pca.frequency = 1600
 
 mtr1_channel = pca.channels[8]
 
-while True:
-	mtr1_channel.duty_cycle = 0xFFFF
-	time.sleep(1)
-	mtr1_channel.duty_cycle = 0x0000
-	time.sleep(1)
+mtr1_channel.duty_cycle = 0xFFFF
+time.sleep(1)
+mtr1_channel.duty_cycle = 0x0000
+time.sleep(1)
+
+pca.channels[7].duty_cycle = 0x0000
+time.sleep(1)
+pca.channels[7].duty_cycle = 0xFFFF
