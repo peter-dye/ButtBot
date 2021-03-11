103d549445bb6ffde52d25193f8d61143c450199
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-03-11 11:44:42 -0500

    added smbus

--- mtr_servo.py
+++ mtr_servo.py
@@ -1,7 +1,8 @@
 import board
 import busio
 import adafruit_pca9685
-i2c = busio.I2C(board.SCL, board.SDA)
-pca = adafruit_pca9685.PCA9685(i2c)
+import smbus2 
+bus = smbus2.SMBus(1)
+pca = adafruit_pca9685.PCA9685(bus)
 
 pca.frequency = 1600
