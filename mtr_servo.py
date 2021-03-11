47c39a00c192a987e271a859b9c8e11053bff74d
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-03-11 12:26:05 -0500

    trying to use servokit

--- mtr_servo.py
+++ mtr_servo.py
@@ -2,20 +2,23 @@ import board
 import busio
 import time
 import adafruit_pca9685
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=8)
 
 
 i2c = busio.I2C(board.SCL, board.SDA)
 pca = adafruit_pca9685.PCA9685(i2c)
 
-pca.frequency = 60
+pca.frequency = 1600
 
 mtr1_channel = pca.channels[8]
 
-mtr1_channel.duty_cycle = 0xFFFF
-time.sleep(1)
-mtr1_channel.duty_cycle = 0x0000
-time.sleep(1)
+while True:
+	mtr1_channel.duty_cycle = 0xFFFF
+	time.sleep(1)
+	mtr1_channel.duty_cycle = 0x0000
+	time.sleep(1)
 
-pca.channels[7].duty_cycle = 0x0000
-time.sleep(1)
-pca.channels[7].duty_cycle = 0xFFFF
+	kit.servo[7].angle = 180
+	time.sleep(1)
+	kit.servo[7].angle = 0
