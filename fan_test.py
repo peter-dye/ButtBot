e8ef5902840738203bca0ea09fd874d3eb15c312
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-03-13 12:38:42 -0500

    swtiched polarity of fan enable

--- fan_test.py
+++ fan_test.py
@@ -5,8 +5,10 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(FAN, GPIO.OUT, initial=GPIO.LOW)
-time.sleep(3)
-GPIO.output(FAN, GPIO.HIGH)
+GPIO.setup(FAN, GPIO.OUT, initial=GPIO.HIGH)
 time.sleep(3)
 GPIO.output(FAN, GPIO.LOW)
+print("LOW")
+time.sleep(3)
+GPIO.output(FAN, GPIO.HIGH)
+print("Back high")
