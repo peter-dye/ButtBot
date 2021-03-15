1300106ec9ee3500a6b8aa597d203f5ab377e51b
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-03-14 20:30:59 -0400

    trying to setmode in main_v1

--- main_v1.py
+++ main_v1.py
@@ -7,6 +7,10 @@ import ultrasonic_driver
 import smbus2      
 import path_Q
 import arm_driver
+import Jetson.GPIO as GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
 # Create i2c busses
 ard_bus = smbus2.SMBus(0)
