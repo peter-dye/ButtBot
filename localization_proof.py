3c06bda7837b3f1a338e92dbbcdae4e36c45affd
Peter Dye <pjad98gmail.com>
2021-03-15 19:55:07 -0400

    implemented 4 marker search based localization

--- localization_proof.py
+++ localization_proof.py
@@ -26,13 +26,18 @@ class Location:
         # constant for a given set of markers
         self.L_AB = np.linalg.norm(markers[0]-markers[1])
         self.L_BC = np.linalg.norm(markers[1]-markers[2])
-        self.L_CA = np.linalg.norm(markers[2]-markers[0])
+
+        # self.L_CA = np.linalg.norm(markers[2]-markers[0])
+
+        self.L_CD = np.linalg.norm(markers[2]-markers[3])
+        self.L_DA = np.linalg.norm(markers[3]-markers[0])
+
 
         # A_* are the angles of the triangle formed by markers
         # constant for a given set of markers
-        self.A_A = np.rad2deg(np.arccos(np.dot(markers[1]-markers[0], markers[2]-markers[0])/(self.L_AB*self.L_CA)))
-        self.A_B = np.rad2deg(np.arccos(np.dot(markers[0]-markers[1], markers[2]-markers[1])/(self.L_AB*self.L_BC)))
-        self.A_C = np.rad2deg(np.arccos(np.dot(markers[0]-markers[2], markers[1]-markers[2])/(self.L_CA*self.L_BC)))
+        # self.A_A = np.rad2deg(np.arccos(np.dot(markers[1]-markers[0], markers[2]-markers[0])/(self.L_AB*self.L_CA)))
+        # self.A_B = np.rad2deg(np.arccos(np.dot(markers[0]-markers[1], markers[2]-markers[1])/(self.L_AB*self.L_BC)))
+        # self.A_C = np.rad2deg(np.arccos(np.dot(markers[0]-markers[2], markers[1]-markers[2])/(self.L_CA*self.L_BC)))
         return
 
     def compute_thetas(self, phi_A, phi_B, phi_C) -> None:
@@ -73,6 +78,38 @@ class Location:
             print("You forgot a case")
 
         return
+
+    def compute_four_thetas(self, phi_A, phi_B, phi_C, phi_D):
+        # convert all angles to cw
+        if phi_A < 0:
+            phi_A = 360 + phi_A
+        if phi_B < 0:
+            phi_B = 360 + phi_B
+        if phi_C < 0:
+            phi_C = 360 + phi_C
+        if phi_D < 0:
+            phi_D = 360 + phi_D
+
+        # get min to determine general orientation
+        phi_min = np.amin(np.array([phi_A, phi_B, phi_C, phi_D]))
+
+        # calculate generic case for all thetas
+        theta_AB = phi_B - phi_A
+        theta_BC = phi_C - phi_B
+        theta_CD = phi_D - phi_C
+        theta_DA = phi_A - phi_D
+
+        # one theta does not fit generic case based on orientation
+        if phi_min == phi_A:
+            theta_DA = 360 - phi_D + phi_A
+        elif phi_min == phi_B:
+            theta_AB = 360 - phi_A + phi_B
+        elif phi_min == phi_C:
+            theta_BC = 360 - phi_B + phi_C
+        elif phi_min == phi_D:
+            theta_CD = 360 - phi_C + phi_D
+
+        return np.array([theta_AB, theta_BC, theta_CD, theta_DA])
 
     def is_inside(self) -> bool:
         """
@@ -213,99 +250,238 @@ class Location:
 
         return
 
+    def search_localize(self, phi_A, phi_B, phi_C):
+        self.compute_thetas(phi_A, phi_B, phi_C)
+
+        # change any thetas over 180 to be 360 - theta
+        if self.theta_AB > 180:
+            self.theta_AB = 360 - self.theta_AB
+        elif self.theta_BC > 180:
+            self.theta_BC = 360 - self.theta_BC
+        elif self.theta_CA > 180:
+            self.theta_CA = 360 - self.theta_CA
+
+        # initialize error to be max
+        best_error = 360
+
+        # loop through all (x, y) coordinates in the space
+        for i in range(200):
+            for j in range(200):
+                # calculate distance to each marker
+                point = np.array([i, j])
+                d_A = np.linalg.norm(point - self.markers[0])
+                d_B = np.linalg.norm(point - self.markers[1])
+                d_C = np.linalg.norm(point - self.markers[2])
+
+                # calculate each theta
+                ctheta_AB = np.rad2deg(np.arccos((self.L_AB**2 - d_A**2 - d_B**2)/(-2*d_A*d_B)))
+                ctheta_BC = np.rad2deg(np.arccos((self.L_BC**2 - d_B**2 - d_C**2)/(-2*d_B*d_C)))
+                ctheta_CA = np.rad2deg(np.arccos((self.L_CA**2 - d_C**2 - d_A**2)/(-2*d_C*d_A)))
+
+                # compute the error for this position
+                error = np.sum(np.array([
+                    np.abs(self.theta_AB - ctheta_AB),
+                    np.abs(self.theta_BC - ctheta_BC),
+                    np.abs(self.theta_CA - ctheta_CA)
+                ]))
+
+                # update location if error is smallest
+                if error < best_error:
+                    best_error = error
+                    location = [i, j]
+                    # print(location)
+
+                # if i == 169 and j == 42:
+                #     print(ctheta_AB, ctheta_BC, ctheta_CA)
+                # if i == 178 and j == 65:
+                #     print(ctheta_AB, ctheta_BC, ctheta_CA)
+
+        return np.array(location)
+
+    def search_four_localize(self, phi_A, phi_B, phi_C, phi_D):
+        (theta_AB, theta_BC, theta_CD, theta_DA) = self.compute_four_thetas(phi_A, phi_B, phi_C, phi_D)
+
+        best_error = 360
+
+        for i in range(200):
+            for j in range(200):
+                # calculate distance to each marker
+                point = np.array([i, j])
+                d_A = np.linalg.norm(point - self.markers[0])
+                d_B = np.linalg.norm(point - self.markers[1])
+                d_C = np.linalg.norm(point - self.markers[2])
+                d_D = np.linalg.norm(point - self.markers[3])
+
+                # calculate each theta
+                ctheta_AB = np.rad2deg(np.arccos((self.L_AB**2 - d_A**2 - d_B**2)/(-2*d_A*d_B)))
+                ctheta_BC = np.rad2deg(np.arccos((self.L_BC**2 - d_B**2 - d_C**2)/(-2*d_B*d_C)))
+                ctheta_CD = np.rad2deg(np.arccos((self.L_CD**2 - d_C**2 - d_D**2)/(-2*d_C*d_D)))
+                ctheta_DA = np.rad2deg(np.arccos((self.L_DA**2 - d_D**2 - d_A**2)/(-2*d_D*d_A)))
+
+                # compute the error for this position
+                error = np.sum(np.array([
+                    np.abs(theta_AB - ctheta_AB),
+                    np.abs(theta_BC - ctheta_BC),
+                    np.abs(theta_CD - ctheta_CD),
+                    np.abs(theta_DA - ctheta_DA)
+                ]))
+
+                # update location if error is smallest
+                if error < best_error:
+                    best_error = error
+                    location = [i, j]
+
+        return np.array(location)
+
 
 # MAIN PROGRAM
-markers = np.array([[173, 109],
-                    [97, 5],
-                    [29, 109]])
+# markers = np.array([[173, 109],
+#                     [97, 5],
+#                     [29, 109]])
+# localizer = Location(markers)
+
+# # case 1
+# phi_A = 54
+# phi_B = 153
+# phi_C = -71
+# coords = localizer.search_localize(phi_A, phi_B, phi_C)
+# print('1. Expected: [83, 74] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+# print('Error: '+str(np.round(np.linalg.norm([83, 74]-coords), 2)))
+#
+# # NOTE: Uncomment the matplotlib import at the top of the file to graph the markers and coordinates
+# # x = [markers[0][0], markers[1][0], markers[2][0], coords[0]]
+# # y = [markers[0][1], markers[1][1], markers[2][1], coords[1]]
+# # plt.scatter(x, y)
+# # plt.show()
+#
+# # case 2
+# phi_A = -54
+# phi_B = 110
+# phi_C = -148
+# coords = localizer.search_localize(phi_A, phi_B, phi_C)
+# print('2. Expected: [115, 44] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+# print('Error: '+str(np.round(np.linalg.norm([115, 44]-coords), 2)))
+#
+# # case 3
+# phi_A = 155
+# phi_B = -70
+# phi_C = 13
+# coords = localizer.search_localize(phi_A, phi_B, phi_C)
+# print('3. Expected: [126, 88] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+# print('Error: '+str(np.round(np.linalg.norm([126, 88]-coords), 2)))
+#
+# # case 4
+# phi_A = -81
+# phi_B = 159
+# phi_C = -148
+# coords = localizer.search_localize(phi_A, phi_B, phi_C)
+# print('4. Expected: [169, 42] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+# print('Error: '+str(np.round(np.linalg.norm([115, 44]-coords), 2)))
+#
+# # case 5
+# phi_A = 15
+# phi_B = -63
+# phi_C = -15
+# coords = localizer.search_localize(phi_A, phi_B, phi_C)
+# print('5. Expected: [192, 97] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+# print('Error: '+str(np.round(np.linalg.norm([192, 97]-coords), 2)))
+#
+# # case 6
+# phi_A = 125
+# phi_B = -35
+# phi_C = 42
+# coords = localizer.search_localize(phi_A, phi_B, phi_C)
+# print('6. Expected: [131, 35] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+# print('Error: '+str(np.round(np.linalg.norm([131, 35]-coords), 2)))
+#
+# # case 7
+# phi_A = -30
+# phi_B = 29
+# phi_C = -100
+# coords = localizer.search_localize(phi_A, phi_B, phi_C)
+# print('7. Expected: [54, 23] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+# print('Error: '+str(np.round(np.linalg.norm([54, 23]-coords), 2)))
+#
+# # case 8
+# phi_A = 137
+# phi_B = -161
+# phi_C = 55
+# coords = localizer.search_localize(phi_A, phi_B, phi_C)
+# print('8. Expected: [34, 71] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+# print('Error: '+str(np.round(np.linalg.norm([34, 71]-coords), 2)))
+#
+# # case 9
+# phi_A = 52
+# phi_B = 102
+# phi_C = -11
+# coords = localizer.search_localize(phi_A, phi_B, phi_C)
+# print('9. Expected: [38, 23] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+# print('Error: '+str(np.round(np.linalg.norm([38, 23]-coords), 2)))
+#
+# # case 10
+# phi_A = -35
+# phi_B = 32
+# phi_C = 96
+# coords = localizer.search_localize(phi_A, phi_B, phi_C)
+# print('10. Expected: [98, 142] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+# print('Error: '+str(np.round(np.linalg.norm([98, 142]-coords), 2)))
+# print(localizer.theta_AB, localizer.theta_BC, localizer.theta_CA)
+#
+# # case 11
+# phi_A = 128
+# phi_B = -164
+# phi_C = -79
+# coords = localizer.search_localize(phi_A, phi_B, phi_C)
+# print('11. Expected: [63, 122] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+# print('Error: '+str(np.round(np.linalg.norm([63, 122]-coords), 2)))
+#
+# # case 12
+# phi_A = -141
+# phi_B = -43
+# phi_C = 19
+# coords = localizer.search_localize(phi_A, phi_B, phi_C)
+# print('12. Expected: [147, 117] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+# print('Error: '+str(np.round(np.linalg.norm([147, 117]-coords), 2)))
+
+# Cases for 4 markers
+markers = np.array([[200, 200],
+                    [200, 0],
+                    [0, 0],
+                    [0, 200]])
 localizer = Location(markers)
 
 # case 1
-phi_A = 54
-phi_B = 153
-phi_C = -71
-coords = localizer.localize(phi_A, phi_B, phi_C)
-print('1. Expected: [83, 74] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
-
-# NOTE: Uncomment the matplotlib import at the top of the file to graph the markers and coordinates
-# x = [markers[0][0], markers[1][0], markers[2][0], coords[0]]
-# y = [markers[0][1], markers[1][1], markers[2][1], coords[1]]
-# plt.scatter(x, y)
-# plt.show()
+phi_A = 46
+phi_B = 120
+phi_C = -164
+phi_D = -56
+coords = localizer.search_four_localize(phi_A, phi_B, phi_C, phi_D)
+print('1. Expected: [70, 123] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+print('Error: '+str(np.round(np.linalg.norm([70, 123]-coords), 2)))
 
 # case 2
-phi_A = -54
-phi_B = 110
-phi_C = -148
-coords = localizer.localize(phi_A, phi_B, phi_C)
-print('2. Expected: [115, 44] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+phi_A = -117
+phi_B = 21
+phi_C = 75
+phi_D = 129
+coords = localizer.search_four_localize(phi_A, phi_B, phi_C, phi_D)
+print('2. Expected: [179, 168] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+print('Error: '+str(np.round(np.linalg.norm([179, 168]-coords), 2)))
 
 # case 3
-phi_A = 155
-phi_B = -70
-phi_C = 13
-coords = localizer.localize(phi_A, phi_B, phi_C)
-print('3. Expected: [126, 88] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+phi_A = -89
+phi_B = -23
+phi_C = 84
+phi_D = -157
+coords = localizer.search_four_localize(phi_A, phi_B, phi_C, phi_D)
+print('3. Expected: [52, 61] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+print('Error: '+str(np.round(np.linalg.norm([52, 61]-coords), 2)))
 
 # case 4
-phi_A = -81
-phi_B = 159
-phi_C = -148
-coords = localizer.localize(phi_A, phi_B, phi_C)
-print('4. Expected: [169, 42] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
-
-# case 5
-phi_A = 15
-phi_B = -63
-phi_C = -15
-coords = localizer.localize(phi_A, phi_B, phi_C)
-print('5. Expected: [192, 97] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
-
-# case 6
-phi_A = 125
-phi_B = -35
-phi_C = 42
-coords = localizer.localize(phi_A, phi_B, phi_C)
-print('6. Expected: [131, 35] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
-
-# case 7
-phi_A = -30
-phi_B = 29
-phi_C = -100
-coords = localizer.localize(phi_A, phi_B, phi_C)
-print('7. Expected: [54, 23] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
-
-# case 8
-phi_A = 137
-phi_B = -161
-phi_C = 55
-coords = localizer.localize(phi_A, phi_B, phi_C)
-print('8. Expected: [34, 71] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
-
-# case 9
-phi_A = 52
-phi_B = 102
-phi_C = -11
-coords = localizer.localize(phi_A, phi_B, phi_C)
-print('9. Expected: [38, 23] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
-
-# case 10
-phi_A = -35
-phi_B = 32
-phi_C = 96
-coords = localizer.localize(phi_A, phi_B, phi_C)
-print('10. Expected: [98, 142] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
-
-# case 11
-phi_A = 128
-phi_B = -164
-phi_C = -79
-coords = localizer.localize(phi_A, phi_B, phi_C)
-print('11. Expected: [63, 122] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
-
-# case 12
-phi_A = -141
-phi_B = -43
-phi_C = 19
-coords = localizer.localize(phi_A, phi_B, phi_C)
-print('12. Expected: [147, 117] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+phi_A = 105
+phi_B = -106
+phi_C = -31
+phi_D = 29
+coords = localizer.search_four_localize(phi_A, phi_B, phi_C, phi_D)
+print('4. Expected: [172, 100] Found: ['+str(round(coords[0], 2))+', '+str(round(coords[1], 2))+']')
+print('Error: '+str(np.round(np.linalg.norm([172, 100]-coords), 2)))
