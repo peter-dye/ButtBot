fd766d8042601fa95492d2a3e13aef6ba396474a
Peter Dye <pjad98gmail.com>
2021-03-17 18:56:37 -0400

    most things being initialized

--- buttbot.py
+++ buttbot.py
@@ -1,3 +1,50 @@
 """
 This is the entry point to running the entire ButtBot system.
 """
+
+from motor_driver import MotorDriver
+from arm_driver import ArmDriver
+from servo_driver import ServoDriver
+from jetcam.csi_camera import CSICamera
+from localizer import Localizer
+from ultrasonic_driver import UltrasonicDriver
+
+
+class ButtBot():
+
+    def __init__(self):
+        # initialize motor driver
+        self.motor_driver = MotorDriver()
+
+        # initialize arm driver
+        self.arm_driver = ArmDriver()
+
+        # initialize servo driver
+        self.servo_driver = ServoDriver()
+
+        # initialize camera
+        self.camera = CSICamera(
+            width=320,
+            height=320,
+            capture_width=1080,
+            capture_height=720,
+            capture_fps=30
+        )
+
+        # initialize localizer
+        markers = []
+        self.localizer = Localizer(
+            markers,
+            self.camera,
+            self.servo_driver,
+            self.arm_driver
+        )
+
+        # initialize ultrasonic sensors (process)
+        self.ultrasonic_driver = UltrasonicDriver()
+
+        # initalize butt detection (process)
+
+        # initialize path planning
+
+        return
