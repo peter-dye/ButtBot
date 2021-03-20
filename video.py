df16397f89569ca3a6ab367e5fafc638645679a5
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-03-20 15:54:24 -0400

    wrote code for video demonstration

---
+++ video.py
@@ -0,0 +1,18 @@
+from motor_driver import MotorDriver
+from arm_driver import ArmDriver
+import time
+
+
+def run():
+    md = MotorDriver()
+
+    while True:
+        time.sleep(10)
+        md.motor_send(1, 10, 'fwd')
+        time.sleep(10)
+
+def arun():
+    arm = ArmDriver()
+    arm.pickup()
+
+    
