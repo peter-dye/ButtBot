47f5cf0f4bf56f5190d84b1a63f0393d5d4e6077
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-03-22 20:43:17 -0400

    testing motor control in timelapse.py

--- timelapse.py
+++ timelapse.py
@@ -1,6 +1,6 @@
 from motor_driver import MotorDriver
-from arm_driver import ArmDriver
-from servo_driver import ServoDriver
+#from arm_driver import ArmDriver
+#from servo_driver import ServoDriver
 import time
 import random as r
 
@@ -9,30 +9,30 @@ sd = ServoDriver()
 ad = ArmDriver()
 
 while True:
-    dur = r.randint(1,5)
-    print("dur is", dur)
+    dist = r.randint(15,50)
+    print("dist is", dist)
     speed = r.uniform(0.1,1)
     print("speed is ", speed)
-    md.motor_send(speed, dur, 'fwd')
+    md.motor_send(speed, dist, 'fwd')
     time.sleep(dur)
 
-    func = r.randint(1,3)
+    #func = r.randint(1,3)
    
-    if func == 1:
-        sub_func = r.randint(1,2)
-        if sub_func == 1:
-            if ad.state == 'up':
-                ad.down()
-            else:
-                ad.up()
-        else:
-            ad.pickup()
-    elif func == 2:
-        sub_func = r.randint(1,2)
-        if sub_func == 1:
-            ang = r.randint(-180,180)
-            sd.pan(ang)
-        else:
-            ang = r.randint(0,126)
-            sd.pitch(ang)
+    #if func == 1:
+    #    sub_func = r.randint(1,2)
+    #    if sub_func == 1:
+    #        if ad.state == 'up':
+    #            ad.down()
+    #        else:
+    #            ad.up()
+    #    else:
+    #        ad.pickup()
+    #elif func == 2:
+    #    sub_func = r.randint(1,2)
+    #    if sub_func == 1:
+    #        ang = r.randint(-180,180)
+    #        sd.pan(ang)
+    #    else:
+    #        ang = r.randint(0,126)
+    #        sd.pitch(ang)
     time.sleep(5)
