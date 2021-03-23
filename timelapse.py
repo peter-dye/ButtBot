a5b92c848a03968ecd4047ba75c4889795117c67
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-03-22 20:49:35 -0400

    reinstating all of timelapse.py

--- timelapse.py
+++ timelapse.py
@@ -1,12 +1,12 @@
 from motor_driver import MotorDriver
-#from arm_driver import ArmDriver
-#from servo_driver import ServoDriver
+from arm_driver import ArmDriver
+from servo_driver import ServoDriver
 import time
 import random as r
 
 md = MotorDriver()
-#sd = ServoDriver()
-#ad = ArmDriver()
+sd = ServoDriver()
+ad = ArmDriver()
 
 while True:
     dist = r.randint(15,50)
@@ -16,23 +16,23 @@ while True:
     md.motor_send(speed, dist, 'fwd')
     time.sleep(dist*0.1)
 
-    #func = r.randint(1,3)
+    func = r.randint(1,3)
    
-    #if func == 1:
-    #    sub_func = r.randint(1,2)
-    #    if sub_func == 1:
-    #        if ad.state == 'up':
-    #            ad.down()
-    #        else:
-    #            ad.up()
-    #    else:
-    #        ad.pickup()
-    #elif func == 2:
-    #    sub_func = r.randint(1,2)
-    #    if sub_func == 1:
-    #        ang = r.randint(-180,180)
-    #        sd.pan(ang)
-    #    else:
-    #        ang = r.randint(0,126)
-    #        sd.pitch(ang)
+    if func == 1:
+        sub_func = r.randint(1,2)
+        if sub_func == 1:
+            if ad.state == 'up':
+                ad.down()
+            else:
+                ad.up()
+        else:
+            ad.pickup()
+    elif func == 2:
+        sub_func = r.randint(1,2)
+        if sub_func == 1:
+            ang = r.randint(-180,180)
+            sd.pan(ang)
+        else:
+            ang = r.randint(0,126)
+            sd.pitch(ang)
     time.sleep(5)
