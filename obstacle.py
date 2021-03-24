c89687c5cb569a77162d288486367f241939355c
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-03-24 18:37:44 -0400

    instating motors

--- obstacle.py
+++ obstacle.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 
 from ultrasonic_driver import UltrasonicDriver
-#from motor_driver import MotorDriver
+from motor_driver import MotorDriver
 import time
 
-#md = MotorDriver()
+md = MotorDriver()
 ud = UltrasonicDriver()
 
 for i in range(5):
-    #md.motor_send(1,100,'fwd')
+    md.motor_send(1,100,'fwd')
     while True:
         distances = ud.get_distances()
         print(distances)
         if distances[0] < 20 or distances[1] < 20:
-            #md.stop()
+            md.stop()
             while distances[0] < 20 or distances[1] < 20:
                 distances = ud.get_distances()
                 print(distances)
