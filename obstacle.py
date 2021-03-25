76be6d1358b561724956d0ea42e5a265094d6b72
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-03-24 20:24:57 -0400

    changed obstacle code

--- obstacle.py
+++ obstacle.py
@@ -10,15 +10,7 @@ ud = UltrasonicDriver()
 for i in range(1):
     print('sending', i)
     md.motor_send(1,1000,'fwd')
-    while md.running():
-        distances = ud.get_distances()
-        print(distances[0],distances[1])
-        if distances[0] < 30 or distances[1] < 30:
-            md.stop()
-            while distances[0] < 30 or distances[1] < 30:
-                distances = ud.get_distances()
-                print(distances[0],distances[1])
-            break
+ 
 print('stopping')
 md.stop()
 
